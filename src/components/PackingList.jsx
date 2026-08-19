f4cefa30d62110a703a6c4c1d305055cddc0b53f
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-08-20 01:39:40 +0530

    implement sorting functionality in PackingList component

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,17 +1,37 @@
-import Item from './Item'
+import { useState } from "react";
+import Item from "./Item";
 
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
 //   { id: 2, description: "Socks", quantity: 12, packed: true },
 // ];
 
-function PackingList({items,onDeleteItem,onToggleItems}) {
+function PackingList({ items, onDeleteItem, onToggleItems }) {
+  const [sortBy,setSortBy] = useState('input')
+  let sortedItems; //derived state
+  if(sortBy === 'input') sortedItems = items;
+  if (sortBy === 'desc') sortedItems = items.slice().sort((a,b)=>a.desc.localeCompare(b.desc)) //using slice() cuz we do not want to mutate the original array by sort()
+  if (sortBy === 'packed') sortedItems = items.slice().sort((a,b)=>Number(a.packed)-Number(b.packed))
   return (
-    <div className='list' >
+    <div className="list">
       <ul>
-        {items.map((item)=><Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItems={onToggleItems} />)}
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItems={onToggleItems}
+          />
+        ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={e=>setSortBy(e.target.value)}>
+          <option value="input">Sort by the input order</option>
+          <option value="desc">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
-    )
-  }
+  );
+}
 export default PackingList;
