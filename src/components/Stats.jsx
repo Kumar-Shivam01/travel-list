c48884f112959614fd56ccace6b46e4044f8c304
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-08-20 01:08:04 +0530

    update Stats component to display item count and packing percentage

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,11 @@
-function Stats() {
+function Stats({items}) {
+  const numItems = items.length;
+  const numPacked = items.filter((item)=>item.packed).length;
+  const percentPacked = Math.round((numPacked/numItems)*100)
+ //const packedItems = items.reduce((acc,curr)=>curr.packed ? acc+1:acc,0)
   return(
     <footer className="stats">
-      <em>You have X items in your list, and you already packed X (X%)</em>
+        <em>{percentPacked === 100 ? 'You got everything! Ready to go ✈️': `You have ${numItems} items in your list, and you already packed ${percentPacked}%`}</em> 
     </footer>
   )
 }
